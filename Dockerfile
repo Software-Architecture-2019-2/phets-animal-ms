22580d36664d4c9108536c0d8f180f521884b2a8
Cristian González <crdgonzalezca@unal.edu.co>
2019-11-18 23:50:22 -0500

    Change default port

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ RUN         pip install pipenv
 COPY        server/ /app/
 WORKDIR     /app
 RUN         pipenv install
+EXPOSE      4003
 CMD         pipenv run python ./run.py
--- server/app/server.py
+++ server/app/server.py
@@ -1,4 +1,8 @@
+import os
+
+_PORT = os.environ.get('PORT') or "4003"
+
 def run():
     from app import app, db, resources
     db.create_all()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=_PORT)
